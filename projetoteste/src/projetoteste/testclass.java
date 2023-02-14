2aa9f9136516712c64cba9abef0f79be26e57f80
Crossfade1 <rui.c.2k11@gmail.com>
2023-02-14 15:58:12 +0000

    commit

--- projetoteste/src/projetoteste/testclass.java
+++ projetoteste/src/projetoteste/testclass.java
@@ -2,8 +2,8 @@ package projetoteste;
 
 public class testclass {
 	
-	public static void main(String[] args) {
-		System.out.println("its here its done");
+	public static void main(String[] args) { 
+		System.out.println("its here");
 	}
 
 }
