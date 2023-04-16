35b6aae925a243a053bd08ac5e143e297aa0d37c
Crossfade1 <rui.c.2k11@gmail.com>
2023-04-17 00:41:55 +0100

    testes

--- projetoteste/src/projetoteste/testclass.java
+++ projetoteste/src/projetoteste/testclass.java
@@ -3,7 +3,7 @@ package projetoteste;
 public class testclass {
 	
 	public static void main(String[] args) { 
-		System.out.println("its here, tes te");
+		System.out.println("its here, te te");
 	}
 
 }
